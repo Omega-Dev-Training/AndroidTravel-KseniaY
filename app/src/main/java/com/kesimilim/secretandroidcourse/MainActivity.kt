fd6627b5004d84bca5799fe2ae3163ba8387c5e7
Ksenia Yunknickel <ksenia.yunknickel@omega-r.com>
2022-08-10 11:06:17 +0300

    Refactoring + add LinearLayout screen

--- app/src/main/java/com/kesimilim/secretandroidcourse/MainActivity.kt
+++ app/src/main/java/com/kesimilim/secretandroidcourse/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.kesimilim.secretandroidcourse
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = Color.TRANSPARENT
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
         printAppInfo()
     }
 
