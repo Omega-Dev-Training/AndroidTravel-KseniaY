0fc03014f9fff55917add931efd9d5953987e9cd
Ksenia Yunknickel <ksenia.yunknickel@omega-r.com>
2022-08-22 15:32:20 +0300

    cinnabon screen part 1

--- app/src/main/java/com/kesimilim/secretandroidcourse/MainActivity.kt
+++ app/src/main/java/com/kesimilim/secretandroidcourse/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_coordinator)
+        setContentView(R.layout.activity_cinnabon)
 
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
